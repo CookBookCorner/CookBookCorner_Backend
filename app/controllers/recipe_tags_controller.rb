031461e7bb3f6bccfb2070ff77f3ec77274fdde6
Aleja Hale <alejajara2@gmail.com>
2024-04-25 15:29:49 -0700

    Expand backend functionality with new controllers and security enhancements

--- app/controllers/recipe_tags_controller.rb
+++ app/controllers/recipe_tags_controller.rb
@@ -1,2 +1,27 @@
 class RecipeTagsController < ApplicationController
+  before_action :set_recipe_tag, only: [:destroy]
+  before_action :authenticate_user!
+
+  def create
+    @recipe_tag = RecipeTag.new(recipe_tag_params)
+    if @recipe_tag.save
+      render json: @recipe_tag, status: :created
+    else
+      render json: @recipe_tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe_tag.destroy
+    head :no_content
+  end
+
+  private
+  def set_recipe_tag
+    @recipe_tag = RecipeTag.find(params[:id])
+  end
+
+  def recipe_tag_params
+    params.require(:recipe_tag).permit(:recipe_id, :tag_id)
+  end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,44 @@
 class TagsController < ApplicationController
+  before_action :set_tag, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @tags = Tag.all
+    render json: @tags
+  end
+
+  def show
+    render json: @tag
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      render json: @tag, status: :created, location: @tag
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @tag.update(tag_params)
+      render json: @tag
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tag.destroy
+    head :no_content
+  end
+
+  private
+    def set_tag
+      @tag= Tag.find(params[:id])
+    end
+    
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
 end
